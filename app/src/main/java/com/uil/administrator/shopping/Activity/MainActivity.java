8c4088b9846f397abf48c3c23d85840a033d2939
kunshao9799 <18179042682@163.com>
2016-03-07 15:42:03 +0800

    添加接口

--- app/src/main/java/com/uil/administrator/shopping/Activity/MainActivity.java
+++ app/src/main/java/com/uil/administrator/shopping/Activity/MainActivity.java
@@ -1,19 +1,18 @@
 package com.uil.administrator.shopping.Activity;
 
 
+import com.uil.administrator.shopping.Adapter.BaseActivity;
 import com.uil.administrator.shopping.Adapter.BaseFragment;
 import com.uil.administrator.shopping.R;
 
 /**
  *  首页
  */
-public class MainActivity extends BaseFragment {
+public class MainActivity extends BaseActivity {
 
 
     @Override
-    protected int contentResid() {
+    protected int contentView() {
         return R.layout.activity_main;
     }
-
-
 }
